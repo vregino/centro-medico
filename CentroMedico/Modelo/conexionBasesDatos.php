448e82d2b7ddd193cf6994da0d4df41ab8b55a24
victor <victor-regino@outlook.com>
2022-01-30 13:05:57 -0500

    se valida conexion base de datos

--- CentroMedico/Modelo/conexionBasesDatos.php
+++ CentroMedico/Modelo/conexionBasesDatos.php
@@ -1,7 +1,7 @@
 <?php
 function Conectarse()
 {
-	$objConexion = new mysqli("localhost","root","sena","centromedico");
+	$objConexion = new mysqli("localhost","root","12345678","centromedico");
 	if ($objConexion->connect_errno)
 	{
 		echo "Erro de conexion a la Base de Datos ".$objConexion->connect_error;
